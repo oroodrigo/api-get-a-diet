38d8065ba4556a0a588ff3d5e9a282aec73d8a68
Rodrigo Oliveira <rodrigofeitosa.contato@gmail.com>
2024-03-27 17:56:17 -0300

    feat: add firebase

--- src/env/index.ts
+++ src/env/index.ts
@@ -3,6 +3,12 @@ import { z } from 'zod'
 const envSchema = z.object({
   NODE_ENV: z.enum(['dev', 'production', 'test']).default('dev'),
   PORT: z.coerce.number().default(3333),
+  API_KEY: z.string(),
+  AUTH_DOMAIN: z.string(),
+  PROJECT_ID: z.string(),
+  STORAGE_BUCKET: z.string(),
+  MESSAGING_SENDER_ID: z.string(),
+  APP_ID: z.string(),
 })
 
 const _env = envSchema.safeParse(process.env)
