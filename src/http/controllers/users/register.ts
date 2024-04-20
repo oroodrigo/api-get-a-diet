4e123417ce51ed51d133b706e89c7f97be1fe193
Rodrigo Oliveira <rodrigofeitosa.contato@gmail.com>
2024-04-20 15:28:12 -0300

    feat: register route

---
+++ src/http/controllers/users/register.ts
@@ -0,0 +1,29 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+import { makeRegisterService } from '../../../services/factories/make-register-service'
+import { UserAlreadyExistsError } from '../../../services/errors/user-already-exists-error'
+
+export async function register(request: FastifyRequest, reply: FastifyReply) {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+    crn: z.number().optional(),
+  })
+
+  const { name, email, password, crn } = registerBodySchema.parse(request.body)
+
+  try {
+    const registerService = makeRegisterService()
+
+    await registerService.execute({ name, email, password, crn })
+  } catch (err) {
+    if (err instanceof UserAlreadyExistsError) {
+      reply.status(409).send({ message: err.message })
+    }
+
+    throw err
+  }
+
+  reply.status(201).send()
+}
