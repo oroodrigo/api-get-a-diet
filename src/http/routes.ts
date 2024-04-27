df7deb11822ec4f9ed5135e169499329a0604910
Rodrigo Oliveira <rodrigofeitosa.contato@gmail.com>
2024-04-27 12:15:22 -0300

    refactor: change user router file location

--- src/http/routes.ts
+++
@@ -1,6 +0,0 @@
-import { FastifyInstance } from 'fastify'
-import { register } from './controllers/users/register'
-
-export async function usersRoute(app: FastifyInstance) {
-  app.post('/users', register)
-}
