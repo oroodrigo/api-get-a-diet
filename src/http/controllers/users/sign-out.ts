6d5f9c121ddae6958c13db8d36a377ddf18e4b59
Rodrigo Oliveira <rodrigofeitosa.contato@gmail.com>
2024-06-25 18:54:35 -0300

    fix: fix async in signout function

--- src/http/controllers/users/sign-out.ts
+++ src/http/controllers/users/sign-out.ts
@@ -1,6 +1,6 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
 
-export function signOut(request: FastifyRequest, reply: FastifyReply) {
+export async function signOut(request: FastifyRequest, reply: FastifyReply) {
   return reply
     .clearCookie('get-a-diet.refreshToken', { path: '/', httpOnly: true })
     .status(200)
